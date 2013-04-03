6f0bdc9ff3f503741e9b0a466ea84e15073c745f
Owen Stephens <owen@ostephens.com>
2013-04-03 16:11:20 +0100

    Uses SID based on hostname

--- local/references/linking.php
+++ local/references/linking.php
@@ -232,7 +232,9 @@ class linking{
                 }
             }
         }
-        $URL .= "&amp;rfr_id=info:sid/[local data - VLE URL]%3A$idname";
+        // Create SID based on host name of Moodle install
+        $sid = str_replace("http://","",$CFG->wwwroot);
+        $URL .= "&amp;rfr_id=info:sid/$sid]%3A$idname";
 
         // Document Genre
         if ($DocType == 0) { $URL .= "&amp;rft.genre=article"; }
