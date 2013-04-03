f38e4e42337967c7327f7353c97561d99a3edda9
Owen Stephens <owen@ostephens.com>
2013-04-03 16:54:44 +0100

    Amended so cron runs once a day at approx 1:30am

--- local/references/lib.php
+++ local/references/lib.php
@@ -1,6 +1,23 @@
 <?php
 
 function local_references_cron() {
+	$modconfig = get_config('local_references');
+	$timenow = time();
+	// Check if cron has run in last 23 hours, if so skip it
+	if ($timenow < $modconfig->lastcron + 23*60*60) {
+        mtrace("Skipping local_references cron as already run today");
+		return;
+    } else {
+		mtrace("Cron not yet run today");
+	}
+	// Check if it's before 1am (most recent midnight + 3600 seconds), if so skip it
+    if ($timenow < strtotime('00:00') + 3630) {
+        mtrace("Skipping local_references cron as not after 1:30am");
+		return;
+	} else {
+		mtrace("It is after 1:30am today so local_references cron can run");
+	}
+	// Cron hasn't run for at least 23 hours, and it's after 1am, so run cron, unless it's set to never run
 	global $CFG;
 	require_once(dirname(__FILE__).'/apibib/apibib_lib.php');
 	if ($cleartemp = apibib::cleartemp()) {
@@ -8,4 +25,6 @@ function local_references_cron() {
 	} else {
 		mtrace('Unable to run clear temp successfully');
 	}
+	// Update the lastcron time
+	set_config('lastcron', $timenow, 'local_references');
 }
