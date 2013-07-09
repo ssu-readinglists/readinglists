be1c2eb8b0e7269d65317b293ece38c933fb244c
Owen Stephens <owen@ostephens.com>
2013-07-09 20:52:24 +0100

    Change default sort order when viewing references

--- mod/refworks/viewrefs.php
+++ mod/refworks/viewrefs.php
@@ -75,7 +75,7 @@ if ($count==0 || $count===false) {
 }
 
 //get if optional sorting param set by user
-$sorting = optional_param('sort',9,PARAM_INT);
+$sorting = optional_param('sort',0,PARAM_INT);
 //see what page we are on or if we are all (from optional params)
 $showall = optional_param('all',false,PARAM_BOOL);
 $curpage = optional_param('page',1,PARAM_INT);
