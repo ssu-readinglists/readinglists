cc811acd7e9706311e1f41f2637a00b6cccd16a2
Owen Stephens <owen@ostephens.com>
2015-05-05 22:46:05 +0100

    Corrected init for course module view event

--- mod/readinglist/classes/event/course_module_viewed.php
+++ mod/readinglist/classes/event/course_module_viewed.php
@@ -25,7 +25,8 @@ namespace mod_readinglist\event;
 defined('MOODLE_INTERNAL') || die();
 class course_module_viewed extends \core\event\course_module_viewed {
     protected function init() {
-        $this->data['objecttable'] = 'readinglist';
-        parent::init();
+    	$this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'mod_readinglist';
     }
 }
