feedbfa7cac48f623aa0b79cd410e5d114b315dd
Owen Stephens <owen@ostephens.com>
2016-05-23 13:00:55 +0100

    Correct name of objecttable

--- mod/readinglist/classes/event/course_module_viewed.php
+++ mod/readinglist/classes/event/course_module_viewed.php
@@ -27,6 +27,6 @@ class course_module_viewed extends \core\event\course_module_viewed {
     protected function init() {
     	$this->data['crud'] = 'r';
         $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
-        $this->data['objecttable'] = 'mod_readinglist';
+        $this->data['objecttable'] = 'readinglist';
     }
 }
